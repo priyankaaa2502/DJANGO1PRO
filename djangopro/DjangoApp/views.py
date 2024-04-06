e7116090d8584c8c94e9fabde538afbcc3581c9e
priyankaaa2502 <priyankaaa2502@gmail.com>
2024-04-06 12:54:12 +0530

    first commit

---
+++ djangopro/DjangoApp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def temp(request):
+    return render(request,'f1.html')
+
+def temp1(request):
+    return render(request,'f2.html')
+
+def temp2(request):
+    return render(request,'f3.html')
+
+def temp3(request):
+    return render(request,'f4.html')
+
+def temp4(request):
+    return render(request,'f5.html')
